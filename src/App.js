a6c79142171a9c8dd5d2d5e9b925bf976b7b242e
pietro99 <pietro.piccini@hotmail.com>
2022-05-25 13:14:14 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import GameNav from './components/GameNav'
+import TextArea from './components/TextArea'
+
+function App() {
+
+
+  return (
+    <div className="">
+      <TextArea/>      
+    </div>
+  );
+}
+
+export default App;
