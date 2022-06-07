7ec462c8a1a388f1cbafe529bf66ee52d84c4029
pietro99 <pietro.piccini@hotmail.com>
2022-06-07 17:52:46 +0200

    highlight selection fized

--- frontend/src/components/HighlightTask.js
+++ frontend/src/components/HighlightTask.js
@@ -13,9 +13,41 @@ const HighlightTask = (props) => {
 
     //HIGHLIGHT HANDLERS
     function highlightHandler(e) {
-        var sel = window.getSelection().toString();
+        var selStart = e.target.selectionStart
+        var selEnd = e.target.selectionEnd
         var data = props.data 
-        data[sentence]["rational"] = [...data[sentence]["rational"], sel]
+
+        var sel = window.getSelection().toString();
+        console.log(sel)
+        
+        if (sentence.substring(selStart, selEnd) == " "){
+            return
+        }
+
+        var newStart = selStart
+        for(var i = selStart; i >= 0; i--) {
+            if (sentence[i] == " " || i == 0){
+                newStart = i
+                break
+            }
+        }
+        var newEnd = selEnd
+        for(var i = selEnd; i <= sentence.length; i++) {
+            if (sentence[i] == " " || i == sentence.length){
+                newEnd = i
+                break
+            }
+        }
+        var finalSelection = sentence.substring(newStart, newEnd).trim()
+        if (finalSelection[finalSelection.length-1] == "," || finalSelection[finalSelection.length-1] == "." || finalSelection[finalSelection.length-1] == ";"){
+            finalSelection = finalSelection.substring(0, finalSelection.length-1)
+        }
+
+        if(data[sentence]["rational"].includes(finalSelection)){
+            return
+        }
+
+        data[sentence]["rational"] = [...data[sentence]["rational"], finalSelection]
         props.setData(({...data}))
     }
 
