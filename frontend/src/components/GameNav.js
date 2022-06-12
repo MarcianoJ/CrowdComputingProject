e647f00a16e737b7766ba6ac819cdfe230178553
pietro99 <pietro.piccini@hotmail.com>
2022-06-12 13:36:37 +0200

    warning for mobile user added

--- frontend/src/components/GameNav.js
+++ frontend/src/components/GameNav.js
@@ -6,29 +6,32 @@ export default function GameNav(props) {
     const userContext = useContext(UserContext);
     const [cookies, setCookie, removeCookie] = useCookies();
     return(
-        <nav className="navbar bg-light">
-            <div className='home-btn'>
-                <button  className="btn btn-primary" onClick={()=>{props.navigate("/")}}>Home</button>
+        <div>
+            <nav className="navbar bg-light">
+                <div className='home-btn'>
+                    <button  className="btn btn-primary" onClick={()=>{props.navigate("/")}}>Home</button>
+                </div>
+
+                <div className='other-btn'>
+
+
+                {   (userContext.user != null && !cookies.anonymous) ? 
+                    ( 
+                <button className="btn btn-primary" onClick={()=>{removeCookie("token"); userContext.setUser(null)}}>Log out</button> ) 
+                : 
+                (
+                    <div>
+                <button className="btn btn-primary" onClick={()=>{props.navigate("/login")}}>Log in</button>
+                <button className="btn btn-primary" onClick={()=>{props.navigate("/signup")}}>Sign up</button>
+                    </ div>
+                )}
+
+                </div>
+            </nav>
+            <div className="warning-mobile">
+                <h5>The game does not work on mobile devices</h5>
             </div>
+        </div>
 
-            <div className='other-btn'>
-
-
-            {   (userContext.user != null && !cookies.anonymous) ? 
-                ( 
-               <button className="btn btn-primary" onClick={()=>{removeCookie("token"); userContext.setUser(null)}}>Log out</button> ) 
-               : 
-               (
-                <div>
-               <button className="btn btn-primary" onClick={()=>{props.navigate("/login")}}>Log in</button>
-               <button className="btn btn-primary" onClick={()=>{props.navigate("/signup")}}>Sign up</button>
-                </ div>
-               )}
-
-            </div>
-
-            
-
-        </nav>
     )
 }
