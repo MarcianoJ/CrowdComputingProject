6bb544a702f921192665f9aec900135f5e2c186e
MarcianoJ <marci_jorden@msn.com>
2022-06-12 13:22:13 +0200

    Fix host

--- backend/task_platform/config/environments/development.rb
+++ backend/task_platform/config/environments/development.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "cc_task_platform.loophole.site"
+  config.hosts << "cctaskplatformbackend.loophole.site"
 end
